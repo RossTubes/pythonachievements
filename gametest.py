556088949f445c4287aacb915e7ec269abb7e464
Jordan Ross <32171@ma-web.nl>
2020-09-21 11:37:57 +0200

    Hello world

---
+++ gametest.py
@@ -0,0 +1,19 @@
+print("Hello You!, ik ben Jordan.")
+print("Wie ben jij?")
+naam = input("Typ hier je naam>>")
+print("Hello", naam)
+print = input
+print("Ik ben een nieuwkomer op het Mediacollege Amsterdam.") 
+print(" Door een aantal vragen over mij te beantwoorden leer je mij beter kennen. ")
+print("")
+
+input("Voor dat ik naar het Media College Amsterdam kwam, zat ik op de school? \n A.MBO midden nederland in amersfoort \n B.Huizermaat \n C.Amesterdam.")
+antwoord = input("Typ hier uw antwoord:  ")
+if antwoord == "A" or antwoord == "Het Katholieke Scholengemeenschap Hoofddorp":
+    print("Het anwoord is juist!")
+    print("")
+else:
+    print("Het antwoord is onjuist. Het goede antwoord: Het Katholieke Scholengemeenschap Hoofddorp.")
+    print("")
+
+print("")
---
+++ variabelen.py
@@ -0,0 +1,26 @@
+# Variablelen
+naam = "Jordan"  #dit heet een string
+aanuit = true    #dit is een boolean
+puntgetal =5.5   #dit is een float of double
+karakter = '#'
+heelgetal = 123  #dit is een integer
+
+
+#puntgetal = puntgetal * 2
+
+#is commentaar
+
+print(naam)
+print(aanuit)
+print(puntgetal)   #print(True)
+print(restwaarde)
+
+
+uitkomst = heelgetal / puntgetal
+print(uitkomt)
+
+naam = "Tantoe"
+
+stukjeTekst = "Hallo"
+stukjeTekst += " Mijn naam is " + naam
+print(stukjeTekst)
