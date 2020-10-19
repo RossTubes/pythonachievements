34cd48b4fefa2cc4f3b7b71d1aa2a4966121f256
Jordan Ross <32171@ma-web.nl>
2020-10-19 10:15:59 +0200

    statistics

---
+++ PYTB1L2PlayerStatistics.py
@@ -0,0 +1,25 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Name = "Name: Jordan"
+>>> print (Name)
+Name: Jordan
+>>> healthbar = "healthbar: 200"
+>>> attack = "attack: Fire breath"
+>>> movementspeed = "Movementspeed: 150"
+>>> lives = "number of lives left 2"
+>>> pet = "personal pet: Babydragon"
+>>> level = "Hero level: 75"
+>>> stamina = "Stamina: 16,4 seconds"
+>>> Range = "Range: Medium"
+>>> damage = "Damage: 124"
+>>> Amor = "Amor: +10"
+>>> Armor = "Armor: +10"
+>>> critical = "Critical attack: 39% change"
+>>> reload = "Reload time: 3 seconds"
+>>> print
+<built-in function print>
+>>> print (movementspeed)
+Movementspeed: 150
+>>> print (attack)
+attack: Fire breath
+>>> 
