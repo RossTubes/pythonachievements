97272b19952a9eb99f15062cfb3ee8c8ba01fa6f
Jordan Ross <32171@ma-web.nl>
2020-10-21 14:25:42 +0200

    statistics

---
+++ PYTB1L2PlayerStatistics2.py
@@ -0,0 +1,24 @@
+Name = "Name: Jordan"
+print (Name)
+healthbar = "healthbar: 200"
+print (healthbar)
+attack = "attack: Fire breath"
+print (attack)
+movementspeed = "Movementspeed: 150"
+print (movementspeed)
+lives = "number of lives left 2"
+print (lives)
+pet = "personal pet: Babydragon"
+level = "Hero level: 75"
+stamina =  float(16.4)
+print("Stamina: " + str(stamina) + " seconds")
+Range = "Range: Medium"
+print (Range)
+damage = "Damage: 124"
+print (damage)
+critical = "Critical attack: 39% change"
+print (critical)
+reload = "Reload time: 3 seconds"
+print (reload)
+armor = 200
+print("armor:"+ str(armor))
